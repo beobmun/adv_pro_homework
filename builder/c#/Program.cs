dbaf102231a243b640a5084ff6cfa1f8e395c285
behan <behan@pusan.ac.kr>
2025-04-11 00:46:19 +0900

    builder pattern c#

---
+++ builder/c#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Person
+{
+    public string Name;
+    public int Age;
+    public string Address;
+}
+
+public class PersonBuilder
+{
+    private Person person = new Person();
+
+    public PersonBuilder WithName(string name)
+    {
+        person.Name = name;
+        return this;
+    }
+
+    public PersonBuilder WithAge(int age)
+    {
+        person.Age = age;
+        return this;
+    }
+
+    public PersonBuilder WithAddress(string address)
+    {
+        person.Address = address;
+        return this;
+    }
+
+    public Person Build()
+    {
+        return person;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new PersonBuilder()
+                            .WithName("John Doe")
+                            .WithAge(30)
+                            .WithAddress("123 Main St")
+                            .Build();
+
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Address: {person.Address}");
+    }
+}
