2eae89fc57eabe3dc4c6b324b1d57e8df0ccfe7f
behan <behan@pusan.ac.kr>
2025-04-11 01:05:50 +0900

    factorymethod pattern c#

---
+++ factory_method/c#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public abstract class Animal {
+    public abstract string Speak();
+}
+
+public class Dog : Animal {
+    public override string Speak() {
+        return "Woof!";
+    }
+}
+
+public class Cat : Animal {
+    public override string Speak() {
+        return "Meow!";
+    }
+}
+
+public class AnimalFactory {
+    public Animal CreateAnimal(string animalType) {
+        if (animalType == "dog") {
+            return new Dog();
+        } else if (animalType == "cat") {
+            return new Cat();
+        } else {
+            throw new ArgumentException("Unknown animal type");
+        }
+    }
+}
+
+class Program {
+    static void Main() {
+        var factory = new AnimalFactory();
+
+        Animal dog = factory.CreateAnimal("dog");
+        Console.WriteLine(dog.Speak());
+
+        Animal cat = factory.CreateAnimal("cat");
+        Console.WriteLine(cat.Speak());
+    }
+}
